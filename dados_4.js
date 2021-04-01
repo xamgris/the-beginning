cb74f395e943ca38e373102ec825194ac3e98c65
Pedro Henrique barbosa da silva <81392136+xamgris@users.noreply.github.com>
2021-04-01 15:18:31 -0300

    Add files via upload

---
+++ dados_4.js
@@ -0,0 +1,7 @@
+const a = 3;
+
+
+
+a = a + 10; //Nao da certo pois tem uma const!ou seja um valor que n poder ser modificado!
+
+console.log(a); 
---
+++ dados_treinamento.js
@@ -0,0 +1,14 @@
+//Primeiro exemplo
+
+//let preco = 40;
+//let desconto = 20;
+//let precoComDesconto = preco * desconto;
+
+//console.log(preco);
+//console.log(desconto);
+//console.log(precoComDesconto)
+//{console.log(externo + interno)}
+
+let externo = "Crosta";
+let interno = "Nucleo";
+console.log(' Nucleo:' + externo + ' Crosta:'  + interno ); 
