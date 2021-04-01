fcce69902efa9ed0ce32e57aef834a5997239309
Pedro Henrique barbosa da silva <81392136+xamgris@users.noreply.github.com>
2021-04-01 15:17:06 -0300

    Create comentario.js

---
+++ comentario.js
@@ -0,0 +1,11 @@
+// console.log e usado para exibir algo na tela do computador!
+console.log("Passo#03");//Hello World!
+console.log("Passo#02");
+console.log("Passo#01"); 
+
+/*
+Cometario
+ de multiplicas
+ linhas!!
+
+*/
