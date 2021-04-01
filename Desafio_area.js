ad9e92acce914da8fcc6698e6105e5a49a6328fd
Pedro Henrique barbosa da silva <81392136+xamgris@users.noreply.github.com>
2021-04-01 15:22:41 -0300

    Add files via upload

---
+++ Desafio_area.js
@@ -0,0 +1,14 @@
+// PI * raio * raio  
+
+let PI = 3.141592;
+let raio = 10;
+
+let Calculo = (PI * (raio * raio)); //tambem posso adicionar o valor completo de PI adicioando Math antes de PI: let Calculo = (Math.PI * (raio * raio));
+
+//outro exemplo
+
+let PI = 3.141592;
+const raio =10;
+let areaCirc = PI * raio * raio
+
+console.log("O valor da area e " + areaCirc + " m2")
---
+++ senteca.js
@@ -0,0 +1,3 @@
+console.log("bom dia!");
+console.log("bom tarde!");
+console.log("boa noite!");
